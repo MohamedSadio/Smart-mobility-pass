a305cacafc1012acc5bb746ec645c6855870be82
Mohas <Mohasadio2002@gmail.com>
2026-02-18 17:31:13 +0000

    m

---
+++ config-server/user-mobility-pass-service/src/main/java/com/smartmobility/usermobilitypassservice/UserMobilityPassServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartmobility.usermobilitypassservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMobilityPassServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMobilityPassServiceApplication.class, args);
+    }
+
+}
---
+++ config-server/user-mobility-pass-service/src/test/java/com/smartmobility/usermobilitypassservice/UserMobilityPassServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmobility.usermobilitypassservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMobilityPassServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
