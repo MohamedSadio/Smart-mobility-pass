a677bbabf3be592160950a8bb5d7dd949366caf8
Mohas <Mohasadio2002@gmail.com>
2026-02-22 22:38:28 +0000

    m

---
+++ api-gateway/src/main/java/com/smartmobility/apigateway/config/SpringCloudGatewayConfiguration.java
@@ -0,0 +1,30 @@
+package com.smartmobility.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringCloudGatewayConfiguration {
+    @Bean
+    RouteLocator gatewayRouter (RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("myHeaderParam","headerParamValue")
+                                .addRequestParameter("myParam","paramValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p->p
+                        .path("/user-mobility-pass/**")
+                        .uri("lb://user-mobility-pass-service"))
+
+                .build();
+    }
+}
