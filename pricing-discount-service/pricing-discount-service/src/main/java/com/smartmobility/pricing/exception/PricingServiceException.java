639c42a01580f006fc028bbcd970a4da1822b80d
Mohas <Mohasadio2002@gmail.com>
2026-02-26 20:06:42 +0000

    m

--- pricing-discount-service/pricing-discount-service/src/main/java/com/smartmobility/pricing/exception/PricingServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.smartmobility.pricing.exception;
-
-public class PricingServiceException extends RuntimeException {
-    public PricingServiceException(String message) {
-        super(message);
-    }
-}
