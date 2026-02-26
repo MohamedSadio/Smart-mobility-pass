a1792aabc9ef96c0b75215f7f9e3a37071cb2692
Mohas <Mohasadio2002@gmail.com>
2026-02-26 18:07:45 +0000

    m

--- pricing-discount-service/pricing-discount-service/src/main/java/com/smartmobility/pricing/controller/PricingController.java
+++ pricing-discount-service/pricing-discount-service/src/main/java/com/smartmobility/pricing/controller/PricingController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/pricing")
+@RequestMapping("/pricing-discount/pricing")
 @RequiredArgsConstructor
 @Slf4j
 public class PricingController {
