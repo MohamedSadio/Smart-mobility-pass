44c9959f7f80903f7fe5a8ff422d11cf3a261bf6
Mohas <Mohasadio2002@gmail.com>
2026-02-27 02:38:54 +0000

    m

---
+++ pricing-discount-service/src/main/java/com/smartmobility/pricing/PricingDiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartmobility.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingDiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingDiscountServiceApplication.class, args);
+	}
+
+}
---
+++ pricing-discount-service/src/test/java/com/smartmobility/pricing/PricingDiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmobility.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingDiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
