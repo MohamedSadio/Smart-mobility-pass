8bcd0bcebe1818714f4244de1f788a657686de89
Mohas <Mohasadio2002@gmail.com>
2026-03-01 13:51:57 +0000

    m

--- trip-management-service/trip-management-service/src/main/java/com/smartmobility/trip/entity/TransportType.java
+++
@@ -1,5 +0,0 @@
-package com.smartmobility.trip.entity;
-
-public enum TransportType {
-    BUS, BRT, TER
-}
--- trip-management-service/trip-management-service/src/main/java/com/smartmobility/trip/entity/TripStatus.java
+++
@@ -1,5 +0,0 @@
-package com.smartmobility.trip.entity;
-
-public enum TripStatus {
-    CREATED, PAID, FAILED
-}
